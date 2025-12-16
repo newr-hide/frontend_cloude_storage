7bc75d636e6c502f91aff1b10a37c846a1bb2363
Bondarev Artem <bond210484@mail.ru>
2025-12-16 15:04:06 +0700

    add api refresh-token

--- cloude_storage/src/api/api.js
+++ cloude_storage/src/api/api.js
@@ -12,6 +12,55 @@ export const api = axios.create({
   }
 })
 
+const getTokens = () => {
+  const accessToken = localStorage.getItem('access_token')
+  const refreshToken = localStorage.getItem('refresh_token')
+  return { accessToken, refreshToken }
+}
+
+api.interceptors.response.use(
+  response => response,
+  async error => {
+    const originalRequest = error.config
+    
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true
+      
+      try {
+        const { refreshToken } = getTokens()
+        
+        if (!refreshToken) {
+          throw new Error('Refresh token отсутствует')
+        }
+        
+        const response = await api.post('/token/refresh/', {
+          refresh: refreshToken
+        }, {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          timeout: 5000
+        })
+        
+        const { access, refresh } = response.data
+        
+        localStorage.setItem('access_token', access)
+        localStorage.setItem('refresh_token', refresh)
+        
+        api.defaults.headers.common['Authorization'] = `Bearer ${access}`
+        
+        return api(originalRequest)
+      } catch (refreshError) {
+        console.error('Ошибка обновления токена:', refreshError)
+        localStorage.removeItem('access_token')
+        localStorage.removeItem('refresh_token')
+        window.location.href = '/'
+      }
+    }
+    return Promise.reject(error)
+  }
+)
+
 api.interceptors.request.use(config => {
 
   const token = localStorage.getItem('access_token')
@@ -37,6 +86,7 @@ api.interceptors.request.use(config => {
 
   return config
 })
+
 
 
 export const register = async (data) => {
@@ -73,7 +123,7 @@ export const loginFunction = async (credentials) => {
     localStorage.setItem('refresh_token', refresh)
     localStorage.setItem('user_id', user.id)
     localStorage.setItem('user_login', user.login)
-    localStorage.setItem('user_mail', user.email)
+    localStorage.setItem('user_email', user.email)
     localStorage.setItem('is_admin', user.is_admin)
     
     return {
